aa436200a19e8fc3e9b0d6a91d3840040e11bb9c
Ramudya Fuji Pratama <ramudyafuji21@gmail.com>
2026-08-26 17:09:13 +0700

    project 1

---
+++ ramaproject1/src/main/java/com/mycompany/ramaproject1/Ramaproject1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ramaproject1;
+
+/**
+ *
+ * @author acer
+ */
+public class Ramaproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        
+    }
+}
